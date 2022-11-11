c98b38e41ff18ea04bd88997fbafa0d80e7b77cf
koliskos <77557421+koliskos@users.noreply.github.com>
2022-11-11 10:57:49 -0500

    deleted a couple

--- enumerate.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-# method for making combinations of possible answers
-def make_hyperparam_combos(config, ordered_config_keys):
-  for k in ordered_config_keys:
-    a = list(config.values())
-    # print(len(list(itertools.product(*a))))
-    return list(itertools.product(*a))
